a5cec427c2a9cfceb379aa0195e1a2099e868232
DiabGarren <garrendiab@gmail.com>
2023-06-24 00:56:27 +0200

    Added bishopric functions

--- js/bishopric.js
+++ js/bishopric.js
@@ -1,22 +1,66 @@
-import { getLocalStorage, getUserData, getParam } from "./utils.mjs";
+import {
+    getLocalStorage,
+    getUserData,
+    getParam,
+    setTitle,
+    getAllBishopricDocs,
+    getBishopricDoc,
+    createBishopricDoc,
+    updateBishopricDoc,
+    deleteBishopricDoc,
+    renderBasepage,
+    renderDocPage,
+    renderNewDocPage,
+    renderUpdateDocPage,
+    deleteDoc
+} from './utils.mjs';
 
 const id = getLocalStorage('id');
 const userData = await getUserData(id);
 
-document.title += ` ${userData.firstName} ${userData.lastName}`;
-document.querySelector('#profile').textContent = `${userData.firstName[0]}${userData.lastName[0]}`;
-
+setTitle(userData.firstName, userData.lastName);
 
 let date = getParam('date');
 const newDoc = getParam('new');
+const update = getParam('update');
 
+const meeting = 'bishopric'
 const wrapper = document.querySelector('.wrapper');
 
 if (!id) {
     location = '/rdpUtilities';
-} else {
-    const userData = await getUserData(id);
+}
+
+if (userData.level < '3') {
+    location = '/rdpUtilities/restricted';
+}
+
+if (date && !update && !newDoc) {
+    await renderDocPage(meeting, userData, getBishopricDoc, date, wrapper);
+}
+
+if (!date && !update && !newDoc) {
+    await renderBasepage(meeting, userData, getAllBishopricDocs, wrapper);
+}
+
+if (newDoc && !update) {
     if (userData.level < '3') {
-        location = '/rdpUtilities/restricted';
+        location = '/rdpUtilities/restricted/';
     }
+    await renderNewDocPage(meeting, createBishopricDoc, wrapper, 'New');
+}
+
+if (!newDoc && update) {
+    if (userData.level < '3') {
+        location = '/rdpUtilities/restricted/';
+    }
+    await renderUpdateDocPage(meeting, getBishopricDoc, updateBishopricDoc, date, wrapper, 'Update');
+}
+
+if (newDoc && update) {
+    location = '/rdpUtilities/dashboard/'
+}
+
+if (document.querySelector('#delete')) {
+    await deleteDoc(meeting, deleteBishopricDoc, date, wrapper);
 }
