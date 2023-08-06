8a2a297a4988cd8d596996ac267d2b6f7001eeed
DiabGarren <garrendiab@gmail.com>
2023-08-06 18:14:35 +0200

    Removed useless file

--- js/utils.js
+++
@@ -1,6 +0,0 @@
-export function getParam(param) {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const product = urlParams.get(param);
-    return product;
-}
