6875d4575db39869bafebad64355f42158e33b50
DiabGarren <garrendiab@gmail.com>
2023-08-27 14:42:31 +0200

    Minor style updates

--- js/restricted.js
+++ js/restricted.js
@@ -1,10 +1,13 @@
 import {
     getLocalStorage,
     getUserData,
+    header,
     setProfile
 } from './utils.mjs';
 
 const id = getLocalStorage('id');
 const userData = await getUserData(id);
+
+document.querySelector('header').innerHTML = header('Restricted');
 
 setProfile(userData.firstName, userData.lastName);
